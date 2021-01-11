4952806e7f2eee3c3b914152398ce168e37eb339
Lennart Puck <puck@fzi.de>
2021-01-11 19:05:33 +0100

    Imported upstream version '1.0.0' of 'upstream'

---
+++ launch/sick_safetyscanners2_launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="sick_safetyscanners2",
+            executable="sick_safetyscanners2_node",
+            name="sick_safetyscanners2_node",
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                {"frame_id": "scan",
+                 "sensor_ip": "192.168.1.11",
+                 "host_ip": "192.168.1.9",
+                 "host_udp_port": 0,
+                 "channel": 0,
+                 "channel_enabled": True,
+                 "skip": 0,
+                 "angle_start": 0.0,
+                 "angle_end": 0.0,
+                 "time_offset": 0.0,
+                 "general_system_state": True,
+                 "derived_settings": True,
+                 "measurement_data": True,
+                 "intrusion_data": True,
+                 "application_io_data": True,
+                 "use_persistend_config": True,
+                 "min_intensities": 0.0}
+            ]
+        )
+    ])
